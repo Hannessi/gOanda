78a28fd8d6fa8417494a62f7e5ad4911fc167a47
Hannes Prinsloo <hannes@andile.io>
2019-02-09 10:53:01 +0200

    Created the templates for the bulk of potential http request

--- V20/client.go
+++ V20/client.go
@@ -60,3 +60,363 @@ func (c *Client) GetAccount() (*GetAccountResponse, error) {
 		Account: getAccountResponse.Account,
 	}, nil
 }
+
+type GetAccountSummaryResponse struct {
+	// todo
+}
+
+func (c *Client) GetAccountSummary() (*GetAccountSummaryResponse, error) {
+	_, err := c.Requester.GetAccountSummary(V20.GetAccountSummaryRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetAccountSummaryResponse{}, nil
+}
+
+type GetAccountInstrumentsResponse struct {
+	// todo
+}
+
+func (c *Client) GetAccountInstruments() (*GetAccountInstrumentsResponse, error) {
+	_, err := c.Requester.GetAccountInstruments(V20.GetAccountInstrumentsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetAccountInstrumentsResponse{}, nil
+}
+
+type PatchAccountConfigurationResponse struct {
+	// todo
+}
+
+func (c *Client) PatchAccountConfiguration() (*PatchAccountConfigurationResponse, error) {
+	_, err := c.Requester.PatchAccountConfiguration(V20.PatchAccountConfigurationRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PatchAccountConfigurationResponse{}, nil
+}
+
+type GetInstrumentCandlesResponse struct {
+	// todo
+}
+
+func (c *Client) GetInstrumentCandles() (*GetInstrumentCandlesResponse, error) {
+	_, err := c.Requester.GetInstrumentCandles(V20.GetInstrumentCandlesRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetInstrumentCandlesResponse{}, nil
+}
+
+type GetInstrumentOrderBookResponse struct {
+	// todo
+}
+
+func (c *Client) GetInstrumentOrderBook() (*GetInstrumentOrderBookResponse, error) {
+	_, err := c.Requester.GetInstrumentOrderBook(V20.GetInstrumentOrderBookRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetInstrumentOrderBookResponse{}, nil
+}
+
+type GetInstrumentPositionBookResponse struct {
+	// todo
+}
+
+func (c *Client) GetInstrumentPositionBook() (*GetInstrumentPositionBookResponse, error) {
+	_, err := c.Requester.GetInstrumentPositionBook(V20.GetInstrumentPositionBookRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetInstrumentPositionBookResponse{}, nil
+}
+
+type PostOrderResponse struct {
+	// todo
+}
+
+func (c *Client) PostOrder() (*PostOrderResponse, error) {
+	_, err := c.Requester.PostOrder(V20.PostOrderRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PostOrderResponse{}, nil
+}
+
+type GetOrdersResponse struct {
+	// todo
+}
+
+func (c *Client) GetOrders() (*GetOrdersResponse, error) {
+	_, err := c.Requester.GetOrders(V20.GetOrdersRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetOrdersResponse{}, nil
+}
+
+type GetPendingOrdersResponse struct {
+	// todo
+}
+
+func (c *Client) GetPendingOrders() (*GetPendingOrdersResponse, error) {
+	_, err := c.Requester.GetPendingOrders(V20.GetPendingOrdersRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetPendingOrdersResponse{}, nil
+}
+
+type GetOrderResponse struct {
+	// todo
+}
+
+func (c *Client) GetOrder() (*GetOrderResponse, error) {
+	_, err := c.Requester.GetOrder(V20.GetOrderRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetOrderResponse{}, nil
+}
+
+type PutReplaceOrderResponse struct {
+	// todo
+}
+
+func (c *Client) PutReplaceOrder() (*PutReplaceOrderResponse, error) {
+	_, err := c.Requester.PutReplaceOrder(V20.PutReplaceOrderRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutReplaceOrderResponse{}, nil
+}
+
+type PutCancelOrderResponse struct {
+	// todo
+}
+
+func (c *Client) PutCancelOrder() (*PutCancelOrderResponse, error) {
+	_, err := c.Requester.PutCancelOrder(V20.PutCancelOrderRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutCancelOrderResponse{}, nil
+}
+
+type PutUpdateOrderClientExtensionsResponse struct {
+	// todo
+}
+
+func (c *Client) PutUpdateOrderClientExtensions() (*PutUpdateOrderClientExtensionsResponse, error) {
+	_, err := c.Requester.PutUpdateOrderClientExtensions(V20.PutUpdateOrderClientExtensionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutUpdateOrderClientExtensionsResponse{}, nil
+}
+
+type GetTradesResponse struct {
+	// todo
+}
+
+func (c *Client) GetTrades() (*GetTradesResponse, error) {
+	_, err := c.Requester.GetTrades(V20.GetTradesRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTradesResponse{}, nil
+}
+
+type GetOpenTradesResponse struct {
+	// todo
+}
+
+func (c *Client) GetOpenTrades() (*GetOpenTradesResponse, error) {
+	_, err := c.Requester.GetOpenTrades(V20.GetOpenTradesRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetOpenTradesResponse{}, nil
+}
+
+type GetTradeResponse struct {
+	// todo
+}
+
+func (c *Client) GetTrade() (*GetTradeResponse, error) {
+	_, err := c.Requester.GetTrade(V20.GetTradeRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTradeResponse{}, nil
+}
+
+type PutCloseTradeResponse struct {
+	// todo
+}
+
+func (c *Client) PutCloseTrade() (*PutCloseTradeResponse, error) {
+	_, err := c.Requester.PutCloseTrade(V20.PutCloseTradeRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutCloseTradeResponse{}, nil
+}
+
+type PutUpdateTradeClientExtensionsResponse struct {
+	// todo
+}
+
+func (c *Client) PutUpdateTradeClientExtensions() (*PutUpdateTradeClientExtensionsResponse, error) {
+	_, err := c.Requester.PutUpdateTradeClientExtensions(V20.PutUpdateTradeClientExtensionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutUpdateTradeClientExtensionsResponse{}, nil
+}
+
+type PutUpdateTradeDependentOrdersResponse struct {
+	// todo
+}
+
+func (c *Client) PutUpdateTradeDependentOrders() (*PutUpdateTradeDependentOrdersResponse, error) {
+	_, err := c.Requester.PutUpdateTradeDependentOrders(V20.PutUpdateTradeDependentOrdersRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutUpdateTradeDependentOrdersResponse{}, nil
+}
+
+type GetPositionsResponse struct {
+	// todo
+}
+
+func (c *Client) GetPositions() (*GetPositionsResponse, error) {
+	_, err := c.Requester.GetPositions(V20.GetPositionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetPositionsResponse{}, nil
+}
+
+type GetOpenPositionsResponse struct {
+	// todo
+}
+
+func (c *Client) GetOpenPositions() (*GetOpenPositionsResponse, error) {
+	_, err := c.Requester.GetOpenPositions(V20.GetOpenPositionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetOpenPositionsResponse{}, nil
+}
+
+type GetInstrumentsPositionResponse struct {
+	// todo
+}
+
+func (c *Client) GetInstrumentsPosition() (*GetInstrumentsPositionResponse, error) {
+	_, err := c.Requester.GetInstrumentsPosition(V20.GetInstrumentsPositionRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetInstrumentsPositionResponse{}, nil
+}
+
+type PutClosePositionResponse struct {
+	// todo
+}
+
+func (c *Client) PutClosePosition() (*PutClosePositionResponse, error) {
+	_, err := c.Requester.PutClosePosition(V20.PutClosePositionRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &PutClosePositionResponse{}, nil
+}
+
+type GetTransactionsResponse struct {
+	// todo
+}
+
+func (c *Client) GetTransactions() (*GetTransactionsResponse, error) {
+	_, err := c.Requester.GetTransactions(V20.GetTransactionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTransactionsResponse{}, nil
+}
+
+type GetTransactionResponse struct {
+	// todo
+}
+
+func (c *Client) GetTransaction() (*GetTransactionResponse, error) {
+	_, err := c.Requester.GetTransaction(V20.GetTransactionRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTransactionResponse{}, nil
+}
+
+type GetRangeOfTransactionsResponse struct {
+	// todo
+}
+
+func (c *Client) GetRangeOfTransactions() (*GetRangeOfTransactionsResponse, error) {
+	_, err := c.Requester.GetRangeOfTransactions(V20.GetRangeOfTransactionsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetRangeOfTransactionsResponse{}, nil
+}
+
+type GetTransactionsAfterTransactionResponse struct {
+	// todo
+}
+
+func (c *Client) GetTransactionsAfterTransaction() (*GetTransactionsAfterTransactionResponse, error) {
+	_, err := c.Requester.GetTransactionsAfterTransaction(V20.GetTransactionsAfterTransactionRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTransactionsAfterTransactionResponse{}, nil
+}
+
+type GetTransactionsStreamResponse struct {
+	// todo
+}
+
+func (c *Client) GetTransactionsStream() (*GetTransactionsStreamResponse, error) {
+	_, err := c.Requester.GetTransactionsStream(V20.GetTransactionsStreamRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetTransactionsStreamResponse{}, nil
+}
+
+type GetInstrumentPricingResponse struct {
+	// todo
+}
+
+func (c *Client) GetInstrumentPricing() (*GetInstrumentPricingResponse, error) {
+	_, err := c.Requester.GetInstrumentPricing(V20.GetInstrumentPricingRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetInstrumentPricingResponse{}, nil
+}
+
+type GetPricingStreamResponse struct {
+	// todo
+}
+
+func (c *Client) GetPricingStream() (*GetPricingStreamResponse, error) {
+	_, err := c.Requester.GetPricingStream(V20.GetPricingStreamRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &GetPricingStreamResponse{}, nil
+}
--- V20/requester/V20/requester.go
+++ V20/requester/V20/requester.go
@@ -8,16 +8,178 @@ type Requester interface {
 	// account
 	GetAccounts(GetAccountsRequest) (*GetAccountsResponse, error)
 	GetAccount(GetAccountRequest) (*GetAccountResponse, error)
+	GetAccountSummary(GetAccountSummaryRequest) (*GetAccountSummaryResponse, error)
+	GetAccountInstruments(GetAccountInstrumentsRequest) (*GetAccountInstrumentsResponse, error)
+	PatchAccountConfiguration(PatchAccountConfigurationRequest) (*PatchAccountConfigurationResponse, error)
+
+	// instruments
+	GetInstrumentCandles(GetInstrumentCandlesRequest) (*GetInstrumentCandlesResponse, error)
+	GetInstrumentOrderBook(GetInstrumentOrderBookRequest) (*GetInstrumentOrderBookResponse, error)
+	GetInstrumentPositionBook(GetInstrumentPositionBookRequest) (*GetInstrumentPositionBookResponse, error)
+
+	// orders
+	PostOrder(PostOrderRequest) (*PostOrderResponse, error)
+	GetOrders(GetOrdersRequest) (*GetOrdersResponse, error)
+	GetPendingOrders(GetPendingOrdersRequest) (*GetPendingOrdersResponse, error)
+	GetOrder(GetOrderRequest) (*GetOrderResponse, error)
+	PutReplaceOrder(PutReplaceOrderRequest) (*PutReplaceOrderResponse, error)
+	PutCancelOrder(PutCancelOrderRequest) (*PutCancelOrderResponse, error)
+	PutUpdateOrderClientExtensions(PutUpdateOrderClientExtensionsRequest) (*PutUpdateOrderClientExtensionsResponse, error)
+
+	// trades
+	GetTrades(GetTradesRequest) (*GetTradesResponse, error)
+	GetOpenTrades(GetOpenTradesRequest) (*GetOpenTradesResponse, error)
+	GetTrade(GetTradeRequest) (*GetTradeResponse, error)
+	PutCloseTrade(PutCloseTradeRequest) (*PutCloseTradeResponse, error)
+	PutUpdateTradeClientExtensions(PutUpdateTradeClientExtensionsRequest) (*PutUpdateTradeClientExtensionsResponse, error)
+	PutUpdateTradeDependentOrders(PutUpdateTradeDependentOrdersRequest) (*PutUpdateTradeDependentOrdersResponse, error)
+
+	// positions
+	GetPositions(GetPositionsRequest) (*GetPositionsResponse, error)
+	GetOpenPositions(GetOpenPositionsRequest) (*GetOpenPositionsResponse, error)
+	GetInstrumentsPosition(GetInstrumentsPositionRequest) (*GetInstrumentsPositionResponse, error)
+	PutClosePosition(PutClosePositionRequest) (*PutClosePositionResponse, error)
+
+	// transactions
+	GetTransactions(GetTransactionsRequest) (*GetTransactionsResponse, error)
+	GetTransaction(GetTransactionRequest) (*GetTransactionResponse, error)
+	GetRangeOfTransactions(GetRangeOfTransactionsRequest) (*GetRangeOfTransactionsResponse, error)
+	GetTransactionsAfterTransaction(GetTransactionsAfterTransactionRequest) (*GetTransactionsAfterTransactionResponse, error)
+	GetTransactionsStream(GetTransactionsStreamRequest) (*GetTransactionsStreamResponse, error)
+
+	// pricing
+	GetInstrumentPricing(GetInstrumentPricingRequest) (*GetInstrumentPricingResponse, error)
+	GetPricingStream(GetPricingStreamRequest) (*GetPricingStreamResponse, error)
 }
 
-type GetAccountsRequest struct {}
+type GetAccountsRequest struct{}
 
 type GetAccountsResponse struct {
 	Accounts []definitions.Account `json:"accounts"`
 }
 
-type GetAccountRequest struct {}
+type GetAccountRequest struct{}
 
 type GetAccountResponse struct {
 	Account definitions.Account `json:"account"`
 }
+
+type GetAccountSummaryRequest struct{}
+
+type GetAccountSummaryResponse struct{}
+
+type GetAccountInstrumentsRequest struct{}
+
+type GetAccountInstrumentsResponse struct{}
+
+type PatchAccountConfigurationRequest struct{}
+
+type PatchAccountConfigurationResponse struct{}
+
+type GetInstrumentCandlesRequest struct{}
+
+type GetInstrumentCandlesResponse struct{}
+
+type GetInstrumentOrderBookRequest struct{}
+
+type GetInstrumentOrderBookResponse struct{}
+
+type GetInstrumentPositionBookRequest struct{}
+
+type GetInstrumentPositionBookResponse struct{}
+
+type PostOrderRequest struct{}
+
+type PostOrderResponse struct{}
+
+type GetOrdersRequest struct{}
+
+type GetOrdersResponse struct{}
+
+type GetPendingOrdersRequest struct{}
+
+type GetPendingOrdersResponse struct{}
+
+type GetOrderRequest struct{}
+
+type GetOrderResponse struct{}
+
+type PutReplaceOrderRequest struct{}
+
+type PutReplaceOrderResponse struct{}
+
+type PutCancelOrderRequest struct{}
+
+type PutCancelOrderResponse struct{}
+
+type PutUpdateOrderClientExtensionsRequest struct{}
+
+type PutUpdateOrderClientExtensionsResponse struct{}
+
+type GetTradesRequest struct{}
+
+type GetTradesResponse struct{}
+
+type GetOpenTradesRequest struct{}
+
+type GetOpenTradesResponse struct{}
+
+type GetTradeRequest struct{}
+
+type GetTradeResponse struct{}
+
+type PutCloseTradeRequest struct{}
+
+type PutCloseTradeResponse struct{}
+
+type PutUpdateTradeClientExtensionsRequest struct{}
+
+type PutUpdateTradeClientExtensionsResponse struct{}
+
+type PutUpdateTradeDependentOrdersRequest struct{}
+
+type PutUpdateTradeDependentOrdersResponse struct{}
+
+type GetPositionsRequest struct{}
+
+type GetPositionsResponse struct{}
+
+type GetOpenPositionsRequest struct{}
+
+type GetOpenPositionsResponse struct{}
+
+type GetInstrumentsPositionRequest struct{}
+
+type GetInstrumentsPositionResponse struct{}
+
+type PutClosePositionRequest struct{}
+
+type PutClosePositionResponse struct{}
+
+type GetTransactionsRequest struct{}
+
+type GetTransactionsResponse struct{}
+
+type GetTransactionRequest struct{}
+
+type GetTransactionResponse struct{}
+
+type GetRangeOfTransactionsRequest struct{}
+
+type GetRangeOfTransactionsResponse struct{}
+
+type GetTransactionsAfterTransactionRequest struct{}
+
+type GetTransactionsAfterTransactionResponse struct{}
+
+type GetTransactionsStreamRequest struct{}
+
+type GetTransactionsStreamResponse struct{}
+
+type GetInstrumentPricingRequest struct{}
+
+type GetInstrumentPricingResponse struct{}
+
+type GetPricingStreamRequest struct{}
+
+type GetPricingStreamResponse struct{}
